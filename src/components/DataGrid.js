7ee18abc0d8e536855408cb2ed5b4695195c2db8
rudinesurya <rudinesurya@gmail.com>
2019-12-10 20:58:45 +0000

    fix bugs with sorting

--- src/components/DataGrid.js
+++ src/components/DataGrid.js
@@ -1,37 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Icon, Label, Menu, Table } from 'semantic-ui-react';
+import { Table } from 'semantic-ui-react';
 
-const DataGrid = ({ data }) => (
-  <div>
-    <Table celled>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>ID</Table.HeaderCell>
-          <Table.HeaderCell>Name</Table.HeaderCell>
-          <Table.HeaderCell>Job</Table.HeaderCell>
-          <Table.HeaderCell>Email</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
+const DataGrid = ({ data, selectedColumn, setSelectedColumn, direction }) => {
+  return (
+    <div>
+      <Table sortable celled fixed>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell
+              sorted={selectedColumn === 'id' ? direction : null}
+              onClick={() => setSelectedColumn('id')}
+            >
+              ID
+            </Table.HeaderCell>
 
-      <Table.Body>
-        {data.map(({ id, name, jobDescription, userEmail }) => {
-          return (
-            <Table.Row key={id}>
-              <Table.Cell>{id}</Table.Cell>
-              <Table.Cell>{name}</Table.Cell>
-              <Table.Cell>{jobDescription}</Table.Cell>
-              <Table.Cell>{userEmail}</Table.Cell>
-            </Table.Row>
-          );
-        })}
-      </Table.Body>
-    </Table>
-  </div>
-);
+            <Table.HeaderCell
+              sorted={selectedColumn === 'name' ? direction : null}
+              onClick={() => setSelectedColumn('name')}
+            >
+              Name
+            </Table.HeaderCell>
+
+            <Table.HeaderCell
+              sorted={selectedColumn === 'jobDescription' ? direction : null}
+              onClick={() => setSelectedColumn('jobDescription')}
+            >
+              Job
+            </Table.HeaderCell>
+
+            <Table.HeaderCell
+              sorted={selectedColumn === 'userEmail' ? direction : null}
+              onClick={() => setSelectedColumn('userEmail')}
+            >
+              Email
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {data.map(({ id, name, jobDescription, userEmail }) => {
+            return (
+              <Table.Row key={id}>
+                <Table.Cell>{id}</Table.Cell>
+                <Table.Cell>{name}</Table.Cell>
+                <Table.Cell>{jobDescription}</Table.Cell>
+                <Table.Cell>{userEmail}</Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    </div>
+  );
+};
 
 DataGrid.propTypes = {
-  data: PropTypes.array
+  data: PropTypes.array,
+  selectedColumn: PropTypes.string,
+  setSelectedColumn: PropTypes.func,
+  direction: PropTypes.string
 };
 
 export default DataGrid;
--- src/components/UserDataGrid.js
+++ src/components/UserDataGrid.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { Input, Pagination } from 'semantic-ui-react';
 import DataGrid from './DataGrid';
@@ -6,8 +6,41 @@ import DataGrid from './DataGrid';
 const UserDataGrid = ({ data, rowsCount }) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [prevColumn, setPrevColumn] = useState(null);
+  const [selectedColumn, setSelectedColumn] = useState('id');
+  const [direction, setDirection] = useState(null);
+  const [sortedData, setSortedData] = useState([]);
   const [activePage, setActivePage] = useState(1);
 
+  const handleSelectColumn = useCallback(selectedColumn => {
+    setSelectedColumn(selectedColumn);
+    if (prevColumn !== selectedColumn) {
+      setPrevColumn(selectedColumn);
+      setDirection('ascending');
+    } else {
+      setDirection(direction === 'ascending' ? 'descending' : 'ascending');
+    }
+  });
+
+  useEffect(() => {
+    const temp = [...searchResults].sort((a, b) => {
+      const x = a[selectedColumn];
+      const y = b[selectedColumn];
+
+      if (selectedColumn === 'id') {
+        return x - y;
+      }
+
+      if (x === y) {
+        return 0;
+      }
+      return x > y ? 1 : -1;
+    });
+
+    setSortedData(direction === 'ascending' ? temp : temp.reverse());
+  }, [selectedColumn, direction, searchResults]);
+
+  // Triggered when user enters search term
   useEffect(() => {
     const results = data.filter(item =>
       item.name.toLowerCase().includes(searchTerm)
@@ -16,10 +49,10 @@ const UserDataGrid = ({ data, rowsCount }) => {
     setActivePage(1);
   }, [data, searchTerm]);
 
-  const totalPages = searchResults.length / rowsCount;
+  const totalPages = sortedData.length / rowsCount;
   const indexOfLast = activePage * rowsCount;
   const indexOfFirst = indexOfLast - rowsCount;
-  const activePageData = searchResults.slice(indexOfFirst, indexOfLast);
+  const activePageData = sortedData.slice(indexOfFirst, indexOfLast);
 
   return (
     <div>
@@ -29,7 +62,12 @@ const UserDataGrid = ({ data, rowsCount }) => {
         value={searchTerm}
         onChange={event => setSearchTerm(event.target.value)}
       />
-      <DataGrid data={activePageData} />
+      <DataGrid
+        data={activePageData}
+        selectedColumn={selectedColumn}
+        setSelectedColumn={handleSelectColumn}
+        direction={direction}
+      />
       <Pagination
         boundaryRange={0}
         defaultActivePage={1}
